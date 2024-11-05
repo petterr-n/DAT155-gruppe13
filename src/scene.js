8e3712227ffea29555cc1f5cd01a55aaffad5ea0
pro028 <pro028@uit.no>
2024-11-05 21:44:53 +0100

    start

--- src/scene.js
+++ src/scene.js
@@ -1,12 +1,39 @@
 import * as THREE from 'three';
 
 export function createScene() {
+    const gameWindow = document.getElementById('render-target');
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x777777);
 
-    // Legg til lys
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(50, 50, 50).normalize();
-    scene.add(light);
+    const camera = new THREE.PerspectiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 10000);
+    camera.position.z = 5;
 
-    return scene;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
+    gameWindow.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    function draw() {
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+
+    function start() {
+        renderer.setAnimationLoop(draw);
+    }
+
+    function stop() {
+        renderer.setAnimationLoop(null);
+    }
+
+    return {
+        start,
+        stop
+    }
+
 }
