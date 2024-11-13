ea3ea5dfe115c9810851f21a4ac264dfcf82aac6
pro028 <pro028@uit.no>
2024-11-13 14:20:50 +0100

    mm

--- src/sound.js
+++ src/sound.js
@@ -1,16 +1,23 @@
 import * as THREE from 'three';
 
 export function addBackgroundSound(camera) {
-    const listner = new THREE.AudioListener();
-    camera.add(listner);
+    const listener = new THREE.AudioListener();
+    camera.add(listener);
 
     const audioLoader = new THREE.AudioLoader();
-    const backgroundSound = new THREE.Audio(listner);
+    const backgroundSound = new THREE.Audio(listener);
 
-    audioLoader.load('sound/jungle-nature.mp3', function(buffer) {
-        backgroundSound.setBuffer(buffer);
-        backgroundSound.setLoop(true);
-        backgroundSound.setVolume(0.5);
-        backgroundSound.play();
-    })
+    // Function to start the sound after user interaction
+    function startBackgroundSound() {
+        audioLoader.load('sound/jungle-nature.mp3', function(buffer) {
+            backgroundSound.setBuffer(buffer);
+            backgroundSound.setLoop(true);
+            backgroundSound.setVolume(0.5);
+            backgroundSound.play();
+        });
+    }
+
+    // Add event listener to start sound on user interaction
+    document.addEventListener('click', startBackgroundSound, { once: true });
+
 }
